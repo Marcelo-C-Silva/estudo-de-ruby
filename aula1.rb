979452a9fc928508b1db69067049690d4497abc3
Marcelo <joao69@aluno.senai-ce.org.br>
2024-02-17 14:53:28 -0300

    testando projeto basico basico basico

--- aula1.rb
+++ aula1.rb
@@ -1,6 +1,37 @@
-name =  gets.chomp
-idade = gets.chomp.to_i
+nome = ""
+idade = 0
+peso = 0
+altura = 0
+imc = 0
 
-puts "olá #{name},"
-puts "#{name} tem exatamente, #{idade} anos"
 
+puts "Calculadora de imc"
+puts "Qual seu nome?"
+nome = gets.chomp
+puts " Ola #{nome}, informe sua altura"
+altura = gets.chomp.to_f
+puts "#{nome}, sua altura é de #{altura}"
+puts "agora informe o seu peso"
+peso = gets.chomp.to_f
+puts "#{nome}, seu peso é de #{peso}"
+
+imc = peso / (altura*altura).floor
+
+if imc <= 18.5
+  puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está abaixo do peso!!"
+
+elsif imc <= 24.9
+  puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está com peso ideal!!"
+
+elsif imc <=29.9
+  puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está em sobrepeso!!"
+
+elsif imc <= 34.9
+    puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está com obesidade !! "
+
+elsif imc <= 39.9
+  puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está com obesidade severa!!"
+
+else imc >= 40.0
+  puts "Olá #{nome}, seu IMC é de #{imc}, ou seja, você está com obesidade morbida!!"
+end
